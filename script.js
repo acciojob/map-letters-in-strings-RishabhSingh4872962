961114cdb51cd70a5f62b1b5842cae841d974f80
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-15 11:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function mapLetters(str="") {
+	if (str.length==1) {
+		const s=str.charAt(0);
+		let obj={
+			s:[0]
+		}
+		return obj;
+	}
+	let obj={};
+	for (let i = 0; i < str.length; i++) {
+		const s=str.charAt(i);
+		if (!obj[s]) {
+			obj[s]=[i];
+		}else{
+			obj[s].push(i);
+		}
+		
+	}
+	return obj
+}
+const s=prompt("enter the string");
+alert(mapLetters(s))
